14774234aac49c557c07d951d5ac51881b71f7fe
madsig <madsig@getacademy.no>
2023-11-16 14:46:38 +0100

    initial commit

---
+++ Oppgave321A/Oppgave321A/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Oppgave321A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = new Counter();
+            string text = "something";
+            while (!string.IsNullOrWhiteSpace(text))
+            {
+                text = Console.ReadLine();
+                counter.AddText(text);
+                counter.ShowCounts();
+            }
+        }
+    }
+
+    public class Counter
+    { 
+        private const int Range = 250;
+        private readonly int[] _counts = new int[Range];
+
+        public void AddText(string text)
+        {
+            foreach (var character in text ?? string.Empty)
+            {
+                _counts[(int)character]++;
+            }
+        }
+
+        public void ShowCounts()
+        {
+            for (var i = 0; i < Range; i++)
+            {
+                if (_counts[i] > 0)
+                {
+                    var character = (char)i;
+                    Console.WriteLine(character + " - " + _counts[i]);
+                }
+            }
+        }
+    }
+}
